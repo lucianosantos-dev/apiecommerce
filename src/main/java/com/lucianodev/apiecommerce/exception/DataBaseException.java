d29457b154383bf48c580d1ad8430c4e92f2c0d4
Luciano Santos de Jesus Reis <lucianodev.contato@gmail.com>
2026-09-16 11:39:18 -0300

    feat: add exceção para tratar erros de integridade no Banco

---
+++ src/main/java/com/lucianodev/apiecommerce/exception/DataBaseException.java
@@ -0,0 +1,7 @@
+package com.lucianodev.apiecommerce.exception;
+
+public class DataBaseException extends RuntimeException {
+    public DataBaseException(String message) {
+        super(message);
+    }
+}
